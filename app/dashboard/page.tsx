3b2d0144cfb0ce9b53cc403f4ae55386d79a012e
Gustav Aggeboe <gustavaggeboe@hotmail.com>
2026-02-16 13:44:17 +0100

    Remove unused SVG assets, add routes for dashboard and asd pages, update layouts.

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import {useRouter} from "next/navigation";
+
+export default function Page() {
+
+  const router = useRouter();
+
+  const handleGoToAsd = () => {
+    router.push("/asd");
+  };
+
+  return (
+    <div className="flex flex-1 min-h-screen items-center justify-center font-sans space-x-4">
+      <p>Dashboard</p>
+
+      <button className='bg-zinc-500 rounded-lg py-4 px-8' onClick={handleGoToAsd}>Go to asd</button>
+    </div>
+  );
+}
