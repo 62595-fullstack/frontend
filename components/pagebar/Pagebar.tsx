bfc537a2041c944f03771dc68989286d7093a712
BjarneBjarne <s235107@dtu.dk>
2026-02-18 13:08:15 +0100

    Increased width of pagebar and sidebar

--- components/pagebar/Pagebar.tsx
+++ components/pagebar/Pagebar.tsx
@@ -8,7 +8,7 @@ export default function Pagebar() {
   if (!content) return null;
 
   return (
-    <aside className="w-48 h-screen bg-zinc-200 text-zinc-800 flex flex-col">
+    <aside className="w-72 h-screen bg-zinc-200 text-zinc-800 flex flex-col">
       <div className="bg-zinc-300 px-4 h-[73px] flex items-center font-semibold text-zinc-600 text-xl flex-shrink-0">
         {title}
       </div>
