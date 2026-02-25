5923ff9175cd167781c254c79f4e7075b140e893
christianwienelundquist <s235091@dtu.dk>
2026-02-25 12:17:14 +0100

    Made profilepage

---
+++ app/profile/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import PagebarContent from "@/components/pagebar/PagebarContent";
+
+export default function Page() {
+  return (
+    <div className="flex flex-1 min-h-screen items-center justify-center font-sans space-x-4">
+      <PagebarContent>
+        <ul className="space-y-2">
+          <li>Profile</li>
+        </ul>
+      </PagebarContent>
+      <p>Profile</p>
+    </div>
+  );
+}
