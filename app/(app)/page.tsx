d56212626b1875b8c799ceadf33f94d34ba8721b
Gustav Aggeboe <gustavaggeboe@hotmail.com>
2026-03-23 11:22:20 +0100

    Restore changes

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -4,7 +4,7 @@ import PagebarContent from "@/components/pagebar/PagebarContent";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen items-center font-sans p-8">
+    <div className="bg-bg flex flex-col h-screen items-center font-sans p-8">
       <PagebarContent>
         <ul className="space-y-2">
           <li>Events</li>
